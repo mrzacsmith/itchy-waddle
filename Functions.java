d5f6d2d43d9e9f0197dc03320076171ae0e1f8c7
Zac Smith <zrs3141592@gmail.com>
2015-10-25 22:47:27 -0700

    Initial commit

---
+++ Functions.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometry;
+
+/**
+ *
+ * @author Zac
+ */
+public class Functions {
+    
+    private final double PI = 3.141592;
+    int length;
+    int width;
+    int height;
+    int radius;
+    
+    public void areaLength() {
+       int area;
+       area = length * width;
+       System.out.println("The area of your square is " + area);     
+    
+    }
+    
+    public void areaTriangle() {
+        double area;
+        area =  ((height * width) / 2);
+        System.out.println("The area of your triangle is " + area);
+        
+    }
+    
+    public void areaCircle() {
+        double area;
+        area = radius * radius * PI;
+        System.out.println("The area of your circle is " + area);
+        
+    }
+
+  
+}
---
+++ Geometry.java
@@ -0,0 +1,42 @@
+/*
+ * This is a practice project working with classes.
+ * Second function of this program is learning Git and GitHub
+ * Enjoy learning!
+ */
+package geometry;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zrsmith
+ */
+public class Geometry {
+
+    public static void main(String[] args) {
+    
+        // Declare classes 
+        Functions functions = new Functions();
+        Scanner keyboard = new Scanner(System.in);
+        
+        // Input data from keyboard
+        System.out.print("Enter length: ");
+        functions.length = keyboard.nextInt();
+        
+        System.out.print("Enter width: ");
+        functions.width = keyboard.nextInt();
+        
+        System.out.print("Enter height: ");
+        functions.height = keyboard.nextInt();
+        
+        System.out.print("Enter radius: ");
+        functions.radius = keyboard.nextInt();
+        
+        // Call the Functions class
+        functions.areaLength();
+        functions.areaTriangle();
+        functions.areaCircle();
+        
+    }
+    
+}
